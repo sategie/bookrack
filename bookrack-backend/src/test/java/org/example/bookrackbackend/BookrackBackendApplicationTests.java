08b2e13b026b3d944323968c989ad2c4ec2a50ef
sategie <sategie@yahoo.com>
2024-10-14 21:17:58 +0200

    Initial commit

---
+++ bookrack-backend/src/test/java/org/example/bookrackbackend/BookrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
