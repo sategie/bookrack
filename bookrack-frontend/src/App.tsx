c3a2ee05248757f7b8ea2ef300d71df80f693d08
sategie <sategie@yahoo.com>
2024-10-21 17:28:43 +0200

    Create PastReads component and set it up to display the past read books.

--- bookrack-frontend/src/App.tsx
+++ bookrack-frontend/src/App.tsx
@@ -6,9 +6,11 @@ import axios from "axios";
 import AllBooks from "./components/AllBooks.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BookDetails from "./components/BookDetails.tsx";
+import PastReads from "./components/PastReads.tsx";
 
 function App() {
-  const [books, setBooks] = useState<Book[]>([])
+  const [books, setBooks] = useState<Book[]>([]);
+  const [pastReads, setPastReads] = useState<Book[]>([]);
 
     const fetchBooksData = () => {
       axios.get("/api/books")
@@ -22,11 +24,16 @@ function App() {
         []
     )
 
+    const addToPastReads = (book:Book) => {
+        setPastReads([...pastReads, book]);
+    }
+
   return (
       <>
           <Routes>
               <Route path="/allbooks" element={<AllBooks books={books} />}/>
-              <Route path="/allbooks/:id" element={<BookDetails books={books} />}  />
+              <Route path="/allbooks/:id" element={<BookDetails books={books} addToPastReads={addToPastReads} />}  />
+              <Route path="/pastreads" element={<PastReads books={pastReads}/>}/>
 
           </Routes>
 
---
+++ bookrack-frontend/src/components/PastReads.tsx
@@ -0,0 +1,28 @@
+import {Book} from "../assets/types/Book.ts";
+import BookCard from "./BookCard.tsx";
+
+export type PastReadProps={
+    books:Book[]
+}
+
+
+export default function PastReads({books}:PastReadProps){
+
+    return(
+        <>
+            <div className="container">
+                <h1>Past Reads</h1>
+                {books.length > 0 ? (
+                    <div className="row">
+                        {books.map((book) => (
+                            <BookCard key={book.id} book={book}/>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No books in your past reads</p>
+                )}
+            </div>
+
+        </>
+    )
+}
