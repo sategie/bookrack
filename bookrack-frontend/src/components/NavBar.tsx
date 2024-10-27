8b6b87a253a8342b38050783f01a2a3c0f43efe3
sategie <sategie@yahoo.com>
2024-10-27 23:29:08 +0100

    Adjust navbar to use bootstrap's API.

--- bookrack-frontend/src/components/NavBar.tsx
+++ bookrack-frontend/src/components/NavBar.tsx
@@ -1,14 +1,25 @@
-import {Link} from "react-router-dom";
-import {Nav} from "react-bootstrap";
+import {useEffect, useState} from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 
+export default function NavBar() {
+    const location = useLocation();
+    const [activeKey, setActiveKey] = useState<string>(location.pathname || "");
 
-export default function NavBar(){
+    useEffect(() => {
+        setActiveKey(location.pathname || "");
+    }, [location]);
 
     return (
-        <Nav className={`navbar navbar-expand-lg navbar-dark bg-dark ${styles.stickyNav}`}>
+        <Nav
+            className={`navbar navbar-expand-lg navbar-dark bg-dark ${styles.stickyNav}`}
+            activeKey={activeKey}
+            onSelect={(selectedNav) =>
+                selectedNav && setActiveKey(selectedNav)} // Update state when a NavLink is selected
+        >
             <div className="container-fluid">
-                <Link className={`navbar-brand ${styles.navbarBrand}`} to="/" >BookRack</Link>
+                <Link className={`navbar-brand ${styles.navbarBrand}`} to="/">BookRack</Link>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,23 +28,29 @@ export default function NavBar(){
 
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className={`nav-link ${styles.navLink} ${styles.navItem}`}
-                                  to="/allbooks">All Books</Link>
-                        </li>
+                        <Nav.Item className="nav-item">
+                            <Nav.Link as={Link} to="/allbooks" eventKey="/allbooks"
+                                      className={`${styles.navLink} ${styles.navItem}`}>
+                                All Books
+                            </Nav.Link>
+                        </Nav.Item>
 
-                        <li className="nav-item">
-                            <Link className={`nav-link ${styles.navLink} ${styles.navItem}`}
-                                  to="/pastreads">Past Reads</Link>
-                        </li>
+                        <Nav.Item className="nav-item">
+                            <Nav.Link as={Link} to="/pastreads" eventKey="/pastreads"
+                                      className={`${styles.navLink} ${styles.navItem}`}>
+                                Past Reads
+                            </Nav.Link>
+                        </Nav.Item>
 
-                        <li className="nav-item">
-                            <Link className={`nav-link ${styles.navLink} ${styles.navItem}`}
-                                  to="/futurereads">Future Reads</Link>
-                        </li>
+                        <Nav.Item className="nav-item">
+                            <Nav.Link as={Link} to="/futurereads" eventKey="/futurereads"
+                                      className={`${styles.navLink} ${styles.navItem}`}>
+                                Future Reads
+                            </Nav.Link>
+                        </Nav.Item>
                     </ul>
                 </div>
             </div>
         </Nav>
-    )
+    );
 }
