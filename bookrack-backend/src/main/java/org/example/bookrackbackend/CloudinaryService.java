f636be3e3533ff0457f5a22fcd8dc0e849a1fa90
sategie <sategie@yahoo.com>
2024-11-05 11:29:10 +0100

    Update uploadImage method in CloudinaryService with image.getBytes method.

--- bookrack-backend/src/main/java/org/example/bookrackbackend/CloudinaryService.java
+++ bookrack-backend/src/main/java/org/example/bookrackbackend/CloudinaryService.java
@@ -4,7 +4,6 @@ import com.cloudinary.Cloudinary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -18,8 +17,6 @@ public class CloudinaryService {
 
     @SuppressWarnings("unchecked")
     public String uploadImage(MultipartFile image) throws IOException {
-        File fileToUpload = File.createTempFile("file", null);
-        image.transferTo(fileToUpload);
         Map<String, Object> response = cloudinary.uploader().upload(image.getBytes(), Map.of());
         return response.get("url").toString();
     }
